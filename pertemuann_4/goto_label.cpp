bc670739f841e13b2189db539daf17e3440f5ece
karinaqilanainggolan <162658060+karinaqilanainggolan@users.noreply.github.com>
2024-06-04 05:53:26 -0800

    Add files via upload

---
+++ pertemuann_4/goto_label.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    a: 
+        cout << "Hello World" << endl;
+        goto d;
+    b:
+        cout << "Universitas Sumatera Utara" << endl;
+        return 0;
+    c:
+        cout << "Fasilkom-TI" << endl;
+        goto b;
+    d:
+        cout << "Ilmu Komputer" << endl;
+        goto c;
+
+    int angka = 1;
+    loop:
+        cout << angka << " ";
+        angka++;
+    if(angka <= 10) {
+        goto loop;
+    }
+}
---
+++ pertemuann_4/while.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int angka = 1;
+    while (angka <= 10) {
+        cout << angka << " ";
+        angka++;
+    }
+
+    int angka = 1;
+    while (angka <= 20) {
+        if (angka % 2 == 0) {
+            cout << angka << " ";
+        }
+        angka++;
+    }
+
+}
